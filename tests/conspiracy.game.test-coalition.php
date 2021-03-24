57310248948799948cf5dda4e9944950e7b63845
Guy Baudouin <guythoun@gmail.com>
2021-03-24 15:42:22 +0100

    fix coalition count

--- tests/conspiracy.game.test-coalition.php
+++ tests/conspiracy.game.test-coalition.php
@@ -32,7 +32,6 @@ class ConspiracyTestCoalitionRulebook extends Conspiracy { // this is your game 
     // class tests
     function testCoalition() {
         $coalition = $this->getScoreTopCoalition(1);
-        echo json_encode($coalition, JSON_PRETTY_PRINT)."\n";
         
         
         // example from rulebook
@@ -46,10 +45,12 @@ class ConspiracyTestCoalitionRulebook extends Conspiracy { // this is your game 
         $equal = $equal && $coalition->alreadyCounted[3] == 10;
         $equal = $equal && $coalition->alreadyCounted[4] == 13;
 
-        if ($equal)
+        if ($equal) {
             echo "Test1: PASSED\n";
-        else
+        } else {
             echo "Test1: FAILED\n";
+            echo json_encode($coalition, JSON_PRETTY_PRINT)."\n";
+        }
     }
 
     function testAll() {
@@ -87,7 +88,6 @@ class ConspiracyTestCoalitionNull extends Conspiracy { // this is your game clas
     // class tests
     function testCoalition() {
         $coalition = $this->getScoreTopCoalition(1);
-        echo json_encode($coalition, JSON_PRETTY_PRINT)."\n";
         
         $equal = $coalition->spot == 1;
         $equal = $equal && $coalition->size == 5;
@@ -99,10 +99,12 @@ class ConspiracyTestCoalitionNull extends Conspiracy { // this is your game clas
         $equal = $equal && $coalition->alreadyCounted[3] == 10;
         $equal = $equal && $coalition->alreadyCounted[4] == 13;
 
-        if ($equal)
+        if ($equal) {
             echo "Test1: PASSED\n";
-        else
+        } else {
             echo "Test1: FAILED\n";
+            echo json_encode($coalition, JSON_PRETTY_PRINT)."\n";
+        }
     }
 
     function testAll() {
@@ -140,7 +142,6 @@ class ConspiracyTestCoalitionComplex extends Conspiracy { // this is your game c
     // class tests
     function testCoalition() {
         $coalition = $this->getScoreTopCoalition(1);
-        echo json_encode($coalition, JSON_PRETTY_PRINT)."\n";
         
         $equal = $coalition->spot == 11;
         $equal = $equal && $coalition->size == 5;
@@ -152,10 +153,69 @@ class ConspiracyTestCoalitionComplex extends Conspiracy { // this is your game c
         $equal = $equal && $coalition->alreadyCounted[3] == 15;
         $equal = $equal && $coalition->alreadyCounted[4] == 13;
 
-        if ($equal)
+
+        if ($equal) {
             echo "Test1: PASSED\n";
-        else
+        } else {
             echo "Test1: FAILED\n";
+            echo json_encode($coalition, JSON_PRETTY_PRINT)."\n";
+        }
+    }
+
+    function testAll() {
+        $this->testCoalition();
+    }
+}
+
+class ConspiracyTestCoalitionBackward extends Conspiracy { // this is your game class defined in ggg.game.php
+    function __construct() {
+        // parent::__construct();
+        include '../material.inc.php';// this is how this normally included, from constructor
+    }
+
+    function getLordInSpot(int $player_id, int $spot) {
+        $LORDS_GUILDS = [
+            3, 4, 4, 5, 5,
+             5, 5, 5, 5,
+              4, 4, 4,
+               2, 2,
+                2,
+           ];
+        if ($player_id != 1) {
+            throw new Error('wrong player_id');
+        }
+
+        if (array_key_exists($spot - 1, $LORDS_GUILDS)) {
+            $lord = new stdClass();
+            $lord->guild = $LORDS_GUILDS[$spot - 1];
+            return $lord;
+        } else {
+            return null;
+        }
+    }
+
+    // class tests
+    function testCoalition() {
+        $coalition = $this->getScoreTopCoalition(1);
+        
+        $equal = $coalition->spot == 4;
+        $equal = $equal && $coalition->size == 6;
+        $equal = $equal && $coalition->guild == 5;
+        $equal = $equal && count($coalition->alreadyCounted) == 6;
+        $equal = $equal && $coalition->alreadyCounted[0] == 4;
+        $equal = $equal && $coalition->alreadyCounted[1] == 5;
+        $equal = $equal && $coalition->alreadyCounted[2] == 9;
+        $equal = $equal && $coalition->alreadyCounted[3] == 8;
+        $equal = $equal && $coalition->alreadyCounted[4] == 7;
+        $equal = $equal && $coalition->alreadyCounted[5] == 6;
+
+
+        if ($equal) {
+            echo "Test1: PASSED\n";
+        } else {
+            echo "Test1: FAILED\n";
+            echo json_encode($coalition, JSON_PRETTY_PRINT)."\n";
+        }
     }
 
     function testAll() {
@@ -169,3 +229,5 @@ $test2 = new ConspiracyTestCoalitionNull();
 $test2->testAll();
 $test3 = new ConspiracyTestCoalitionComplex();
 $test3->testAll();
+$test4 = new ConspiracyTestCoalitionBackward();
+$test4->testAll();
